b15e8503c9fb784575c3ef931a5592e073a323ca
peterdidimitrov <petr_dimitrov@abv.bg>
2024-02-28 22:32:27 +0200

    Reorder the code.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/obj/Debug/net6.0/SchoolFoodStamps.Web.ViewModels.AssemblyInfo.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/obj/Debug/net6.0/SchoolFoodStamps.Web.ViewModels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolFoodStamps.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71699437e1cea9079c2cc29eb9a54eb1ce9b880d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7d01e97da0edc6d5d49a647a1e10e011888d730")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolFoodStamps.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolFoodStamps.Web.ViewModels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
