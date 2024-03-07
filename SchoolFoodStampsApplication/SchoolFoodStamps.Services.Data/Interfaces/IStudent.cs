9612c2b5faf1eb8cfbeddc0204c569e87430df12
peterdidimitrov <petr_dimitrov@abv.bg>
2024-03-07 23:29:12 +0200

    Add to IStudent get all method.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IStudent.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IStudent.cs
@@ -1,9 +1,11 @@
-﻿using SchoolFoodStamps.Web.ViewModels.Student;
+﻿using SchoolFoodStamps.Web.ViewModels.CateringCompany;
+using SchoolFoodStamps.Web.ViewModels.Student;
 
 namespace SchoolFoodStamps.Services.Data.Interfaces
 {
     public interface IStudent
     {
         Task CreateAsync(StudentFormViewModel formModel);
+        Task<IEnumerable<StudentFormViewModel>> GetAllStudentsAsync();
     }
 }
