bc0de1e409334f1fc88d762c8e5f459cd425db51
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-20 16:58:38 +0200

    Make methods non-nullable.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.Infrastructure/Extensions/ClaimsPrincipalExtesions.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.Infrastructure/Extensions/ClaimsPrincipalExtesions.cs
@@ -2,13 +2,13 @@
 {
     public static class ClaimsPrincipalExtensions
     {
-        public static string? GetId(this ClaimsPrincipal user)
+        public static string GetId(this ClaimsPrincipal user)
             => user.FindFirstValue(ClaimTypes.NameIdentifier);
 
-        public static string? GetEmail(this ClaimsPrincipal user)
+        public static string GetEmail(this ClaimsPrincipal user)
             => user.FindFirstValue(ClaimTypes.Email);
 
-        public static string? GetRole(this ClaimsPrincipal user)
+        public static string GetRole(this ClaimsPrincipal user)
             => user.FindFirstValue(ClaimTypes.Role);
     }
 }
