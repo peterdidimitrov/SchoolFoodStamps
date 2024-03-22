a79c3abc268c3445dc19e9faadfebd047b3982de
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-22 13:28:06 +0200

    Create AllStudentsFilteredAndPagedServiceModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data.Models/Students/AllStudentsFilteredAndPagedServiceModel.cs
@@ -0,0 +1,16 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Student;
+
+namespace SchoolFoodStamps.Services.Data.Models.Students
+{
+    public class AllStudentsFilteredAndPagedServiceModel
+    {
+        public AllStudentsFilteredAndPagedServiceModel()
+        {
+            Students = new HashSet<StudentViewModel>();
+        }
+
+        public int TotalStudentsCount { get; set; }
+
+        public IEnumerable<StudentViewModel> Students { get; set; }
+    }
+}
