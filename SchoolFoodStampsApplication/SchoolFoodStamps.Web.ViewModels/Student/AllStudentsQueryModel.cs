ea4d4e58890aa1715a77c9528e1c97389fad43b7
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-22 13:24:07 +0200

    Create AllStudentsQueryModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Student/AllStudentsQueryModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolFoodStamps.Web.ViewModels.Student
+{
+    public class AllStudentsQueryModel
+    {
+        public AllStudentsQueryModel()
+        {
+            ClassLetters = new HashSet<ClassLetter>();
+            ClassNumbers = new HashSet<byte>();
+            Students = new HashSet<StudentViewModel>();
+        }
+
+        [Display(Name = "Search by name")]
+        public string? Name { get; set; }
+
+        [Display(Name = "Search by class number")]
+        public string? ClassNumber { get; set; }
+
+        [Display(Name = "Search by class letter")]
+        public string? ClassLetter { get; set; }
+
+        [Display(Name = "Sort Houses By")]
+        public StudentSorting StudentSorting { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        [Display(Name = "Show Students On Page")]
+        public int StudentsPerPage { get; set; }
+
+        public int TotalStudents { get; set; }
+
+        public IEnumerable<ClassLetter> ClassLetters { get; set; }
+
+        public IEnumerable<byte> ClassNumbers { get; set; }
+
+        public IEnumerable<StudentViewModel> Students { get; set; }
+    }
+}
