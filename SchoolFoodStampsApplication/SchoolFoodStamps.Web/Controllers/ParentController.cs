83e2c0a03990fbf755219e164ffa66d3a558d648
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-27 11:17:57 +0200

    Implement edit logic for Parent entity.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web/Controllers/ParentController.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web/Controllers/ParentController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SchoolFoodStamps.Services.Data.Interfaces;
+using SchoolFoodStamps.Web.ViewModels.CateringCompany;
 using SchoolFoodStamps.Web.ViewModels.Parent;
 using System.Security.Claims;
 using static SchoolFoodStamps.Common.NotificationMessagesConstants;
@@ -88,6 +89,65 @@ namespace SchoolFoodStamps.Web.Controllers
             this.TempData[SuccessMessage] = "You are created a profile successfully. Please sign in again.";
 
             return this.RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "Parent")]
+        public async Task<IActionResult> Edit()
+        {
+            string userId = User.GetId();
+
+            ParentFormViewModel? model = await this.parentService.GetParentByUserIdAsync(userId);
+
+            if (model == null)
+            {
+                logger.LogWarning("Pant not found.");
+                this.ModelState.AddModelError(string.Empty, "Unexpected error occurred while trying to add new school! Please try again or contact administrator.");
+
+                return BadRequest();
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Parent")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Edit(ParentFormViewModel formModel)
+        {
+            string userId = User.GetId();
+
+            bool parentExists = await this.parentService.ExistsByUserIdAsync(userId);
+
+            if (!parentExists)
+            {
+                logger.LogWarning("Parent not found.");
+                this.ModelState.AddModelError(string.Empty, "Unexpected error occurred while trying to add new school! Please try again or contact administrator.");
+
+                return BadRequest();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                logger.LogWarning("Model state is not valid.");
+
+                return View(formModel);
+            }
+
+            try
+            {
+                await this.parentService.UpdateAsync(formModel);
+                logger.LogInformation("Parent updated successfully.");
+                this.TempData[SuccessMessage] = "Personal info updated successfully.";
+            }
+            catch (Exception)
+            {
+                this.ModelState.AddModelError(string.Empty, "Unexpected error occurred while trying to add new school! Please try again or contact administrator.");
+
+                return View(formModel);
+            }
+
+            return RedirectToAction("RedirectToIdentityManage", "Home");
         }
     }
 }
