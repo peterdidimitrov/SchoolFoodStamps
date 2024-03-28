1bbdc07cfbcee0c7b76d569181564fb271683714
peterdidimitrov <petr_dimitrov@abv.bg>
2024-03-28 21:43:23 +0200

    Add display attributes to FoodStampSorting.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Enums/FoodStampSorting.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Enums/FoodStampSorting.cs
@@ -1,7 +1,11 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 public enum FoodStampSorting
 {
+    [Display(Name = "Issue date")]
     IssueDate = 0,
+    [Display(Name = "Expiry date")]
     ExpiryDate = 1,
+    [Display(Name = "Renewed date")]
     RenewedDate = 2
 }
