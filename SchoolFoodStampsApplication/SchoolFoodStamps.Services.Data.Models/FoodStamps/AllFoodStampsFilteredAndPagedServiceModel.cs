fcea7a8b3ba0d314ff32f684993881e1f4231f5a
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-28 16:39:58 +0200

    Create AllFoodStampsFilteredAndPagedServiceModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data.Models/FoodStamps/AllFoodStampsFilteredAndPagedServiceModel.cs
@@ -0,0 +1,16 @@
+﻿using SchoolFoodStamps.Web.ViewModels.FoodStamp;
+
+namespace SchoolFoodStamps.Services.Data.Models.FoodStamps
+{
+    public class AllFoodStampsFilteredAndPagedServiceModel
+    {
+        public AllFoodStampsFilteredAndPagedServiceModel()
+        {
+            FoodStamps = new HashSet<FoodStampViewModel>();
+        }
+
+        public int TotalFoodStampsCount { get; set; }
+
+        public IEnumerable<FoodStampViewModel> FoodStamps { get; set; }
+    }
+}
