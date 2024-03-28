7622e0b4eff2b9613f707488428b59371bc9070e
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-28 12:55:36 +0200

    Create FoodStampViewModel

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/FoodStamp/FoodStampViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.FoodStamp
+{
+    public class FoodStampViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+
+        public string IssueDate { get; set; } = string.Empty;
+
+        public string ExpiryDate { get; set; } = string.Empty;
+
+        public string RenewedDate { get; set; } = string.Empty;
+
+        public string Status { get; set; } = string.Empty;
+    }
+}
