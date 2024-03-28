c1ee752b3cd2bab799c8236f294f1d6747bc6987
peterdidimitrov <petr_dimitrov@abv.bg>
2024-03-28 21:42:46 +0200

    Add Display attribute to the HasFoodStamps.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Enums/StudentSorting.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Enums/StudentSorting.cs
@@ -1,7 +1,10 @@
-﻿public enum StudentSorting
+﻿using System.ComponentModel.DataAnnotations;
+
+public enum StudentSorting
 {
     Name = 0,
     Class = 1,
+    [Display(Name = "Has food stamps")]
     HasFoodStamps = 2,
     School = 3
 }
