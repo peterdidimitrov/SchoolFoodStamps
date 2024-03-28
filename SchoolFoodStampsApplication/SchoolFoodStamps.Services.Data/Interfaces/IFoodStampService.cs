722dcad5a8b586bb72e9dcf7628823b9fe8a1fed
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-28 16:40:52 +0200

    Crate IFoodStampService.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IFoodStampService.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IFoodStampService.cs
@@ -1,6 +1,10 @@
-﻿namespace SchoolFoodStamps.Services.Data.Interfaces
+﻿using SchoolFoodStamps.Services.Data.Models.FoodStamps;
+using SchoolFoodStamps.Web.ViewModels.FoodStamp;
+
+namespace SchoolFoodStamps.Services.Data.Interfaces
 {
     public interface IFoodStampService
     {
+        Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByStudentAsync(AllFoodStampsQueryModel queryModel, string studentId);
     }
 }
