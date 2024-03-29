92bd815360de9374fa8760624085737318c3df22
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-29 16:46:09 +0200

    Create DishViewModel

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Dish/DishViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.Dish
+{
+    public class DishViewModel
+    {
+        public DishViewModel()
+        {
+            Allergens = new HashSet<string>();
+        }
+        public string Id { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Weight { get; set; } = string.Empty;
+
+        public IEnumerable<string> Allergens { get; set; }
+    }
+}
