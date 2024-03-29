a14828fd9ce490becc2ef296e6088bfe7ac4ac39
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-03-29 16:44:54 +0200

    Add new properties in FoodStampViewModel: Menu, StudentId, Student name and MenuId.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/FoodStamp/FoodStampViewModel.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/FoodStamp/FoodStampViewModel.cs
@@ -13,5 +13,13 @@
         public string RenewedDate { get; set; } = string.Empty;
 
         public string Status { get; set; } = string.Empty;
+
+        public string Menu { get; set; } = string.Empty;
+
+        public string StudentId { get; set; } = string.Empty;
+
+        public string StudentName { get; set; } = string.Empty;
+
+        public int MenuId { get; set; }
     }
 }
