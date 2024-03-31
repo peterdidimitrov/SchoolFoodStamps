f66d8e6498645a591a4d061398eeea026478f168
peterdidimitrov <petr_dimitrov@abv.bg>
2024-03-31 22:12:01 +0300

    Implement GetAllFoodStampsByParentIdAsync method in FoodStampService.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/FoodStampService.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/FoodStampService.cs
@@ -4,7 +4,10 @@ using SchoolFoodStamps.Data.Models;
 using SchoolFoodStamps.Services.Data.Interfaces;
 using SchoolFoodStamps.Services.Data.Models.FoodStamps;
 using SchoolFoodStamps.Web.ViewModels.FoodStamp;
+using SchoolFoodStamps.Web.ViewModels.School;
+using SchoolFoodStamps.Web.ViewModels.Student;
 using System.Globalization;
+using static SchoolFoodStamps.Common.EntityValidationConstants;
 using static SchoolFoodStamps.Common.GeneralApplicationConstants;
 
 namespace SchoolFoodStamps.Services.Data
@@ -17,7 +20,7 @@ namespace SchoolFoodStamps.Services.Data
         {
             this.repository = repository;
         }
-        public async Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByStudentIdAsync(AllFoodStampsQueryModel queryModel, string studentId)
+        public async Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByStudentIdAsync(AllFoodStampsQueryModel<StudentViewModel> queryModel, string studentId)
         {
             IQueryable<FoodStamp> foodStampQuery = repository
                 .AllReadOnly<FoodStamp>()
@@ -70,17 +73,77 @@ namespace SchoolFoodStamps.Services.Data
             };
         }
 
-        public async Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByParentIdAsync(AllFoodStampsQueryModel queryModel, string parentId)
+        public async Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByParentIdAsync(AllFoodStampsQueryModel<StudentViewModel> queryModel, string parentId)
         {
             IQueryable<FoodStamp> foodStampQuery = repository
             .AllReadOnly<FoodStamp>()
                 .Where(s => s.ParentId == Guid.Parse(parentId))
                 .AsQueryable();
 
-            if (!string.IsNullOrWhiteSpace(queryModel.StudentId))
+            if (!string.IsNullOrWhiteSpace(queryModel.SearchId))
             {
                 foodStampQuery = foodStampQuery
-                    .Where(fs => fs.StudentId == Guid.Parse(queryModel.StudentId));
+                    .Where(fs => fs.StudentId == Guid.Parse(queryModel.SearchId));
+            }
+
+
+            foodStampQuery = queryModel.Status switch
+            {
+                "Valid" => foodStampQuery
+                    .Where(fs => fs.Status == FoodStampStatus.Valid),
+                "Used" => foodStampQuery
+                    .Where(fs => fs.Status == FoodStampStatus.Used),
+                "Renewed" => foodStampQuery
+                    .Where(fs => fs.Status == FoodStampStatus.Renewed),
+                "Expired" => foodStampQuery
+                    .Where(fs => fs.Status == FoodStampStatus.Expired),
+                _ => foodStampQuery
+            };
+
+            foodStampQuery = queryModel.FoodStampSorting switch
+            {
+                FoodStampSorting.IssueDate => foodStampQuery
+                    .OrderBy(s => s.IssueDate),
+                FoodStampSorting.ExpiryDate => foodStampQuery
+                    .OrderBy(s => s.ExpiryDate),
+                FoodStampSorting.RenewedDate => foodStampQuery
+                    .OrderBy(s => s.RenewedDate),
+                _ => foodStampQuery
+                    .OrderBy(s => s.IssueDate)
+            };
+
+            IEnumerable<FoodStampViewModel> allFoodStamps = await foodStampQuery
+                .Skip((queryModel.CurrentPage - 1) * queryModel.FoodStampsPerPage)
+                .Take(queryModel.FoodStampsPerPage)
+                .Select(fs => new FoodStampViewModel
+                {
+                    IssueDate = fs.IssueDate.ToString(DateFormat, CultureInfo.InvariantCulture),
+                    ExpiryDate = fs.ExpiryDate.ToString(DateFormat, CultureInfo.InvariantCulture),
+                    RenewedDate = fs.RenewedDate.HasValue ? fs.RenewedDate.Value.ToString(DateFormat, CultureInfo.InvariantCulture) : null!,
+                    Status = fs.Status.ToString()
+                })
+                .ToListAsync();
+
+            int totalFoodstamps = await foodStampQuery.CountAsync();
+
+            return new AllFoodStampsFilteredAndPagedServiceModel()
+            {
+                TotalFoodStampsCount = totalFoodstamps,
+                FoodStamps = allFoodStamps
+            };
+        }
+
+        public async Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByCateringCompanyIdAsync(AllFoodStampsQueryModel<SchoolViewModel> queryModel, string cateringCompanyId)
+        {
+            IQueryable<FoodStamp> foodStampQuery = repository
+            .AllReadOnly<FoodStamp>()
+                .Where(s => s.CateringCompanyId == Guid.Parse(cateringCompanyId))
+                .AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(queryModel.SearchId))
+            {
+                foodStampQuery = foodStampQuery
+                    .Where(fs => fs.SchoolId == Guid.Parse(queryModel.SearchId));
             }
 
 
