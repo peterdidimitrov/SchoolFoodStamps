7cec5f4c6088caf926f58aa1e41de190c83c2307
peterdidimitrov <petr_dimitrov@abv.bg>
2024-03-31 22:08:31 +0300

    Add GetAllFoodStampsByCateringCompanyIdAsync method in IFoodStampService.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IFoodStampService.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IFoodStampService.cs
@@ -1,11 +1,14 @@
 ﻿using SchoolFoodStamps.Services.Data.Models.FoodStamps;
 using SchoolFoodStamps.Web.ViewModels.FoodStamp;
+using SchoolFoodStamps.Web.ViewModels.School;
+using SchoolFoodStamps.Web.ViewModels.Student;
 
 namespace SchoolFoodStamps.Services.Data.Interfaces
 {
     public interface IFoodStampService
     {
-        Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByStudentIdAsync(AllFoodStampsQueryModel queryModel, string studentId);
-        Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByParentIdAsync(AllFoodStampsQueryModel queryModel, string parentId);
+        Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByStudentIdAsync(AllFoodStampsQueryModel<StudentViewModel> queryModel, string studentId);
+        Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByParentIdAsync(AllFoodStampsQueryModel<StudentViewModel> queryModel, string parentId);
+        Task<AllFoodStampsFilteredAndPagedServiceModel> GetAllFoodStampsByCateringCompanyIdAsync(AllFoodStampsQueryModel<SchoolViewModel> queryModel, string cateringCompanyId);
     }
 }
