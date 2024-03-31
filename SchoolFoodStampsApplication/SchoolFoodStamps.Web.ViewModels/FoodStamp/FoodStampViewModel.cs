8d3e6a4d59153dea905c4a9421020adf7906f72a
peterdidimitrov <petr_dimitrov@abv.bg>
2024-03-31 17:48:34 +0300

    Remove id,  price, menu, studentId, studentName, menuId properties from FoodStampViewModel

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/FoodStamp/FoodStampViewModel.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/FoodStamp/FoodStampViewModel.cs
@@ -2,9 +2,6 @@
 {
     public class FoodStampViewModel
     {
-        public string Id { get; set; } = string.Empty;
-
-        public decimal Price { get; set; }
 
         public string IssueDate { get; set; } = string.Empty;
 
@@ -13,13 +10,5 @@
         public string RenewedDate { get; set; } = string.Empty;
 
         public string Status { get; set; } = string.Empty;
-
-        public string Menu { get; set; } = string.Empty;
-
-        public string StudentId { get; set; } = string.Empty;
-
-        public string StudentName { get; set; } = string.Empty;
-
-        public int MenuId { get; set; }
     }
 }
