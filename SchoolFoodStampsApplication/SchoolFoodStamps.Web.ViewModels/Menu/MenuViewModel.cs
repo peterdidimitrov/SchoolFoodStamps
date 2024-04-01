355454c747404b1e6c55d81c3cbc812a9d368f6c
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-01 22:23:15 +0300

    Create MenuViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Menu/MenuViewModel.cs
@@ -0,0 +1,19 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Dish;
+
+namespace SchoolFoodStamps.Web.ViewModels.Menu
+{
+    public class MenuViewModel
+    {
+        public MenuViewModel()
+        {
+            this.Dishes = new HashSet<DishViewModel>();
+        }
+        public int Id { get; set; }
+
+        public string DayOfWeek { get; set; } = string.Empty;
+
+        public string CateringCompanyId { get; set; } = string.Empty;
+
+        public ICollection<DishViewModel> Dishes { get; set; }
+    }
+}
