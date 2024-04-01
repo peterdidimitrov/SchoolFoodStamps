870c4626e7664bb43f7a454bc8a34af29fe5de62
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-01 22:19:11 +0300

    Create DishController.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web/Controllers/DishController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolFoodStamps.Web.Controllers
+{
+    [Authorize]
+    public class DishController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize(Roles = "CateringCompany")]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }   
+    }
+}
