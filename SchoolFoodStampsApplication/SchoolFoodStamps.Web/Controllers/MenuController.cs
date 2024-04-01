cdaaaf3fa59160b47b1e84178f6fd9af94b1564a
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-01 08:41:09 +0300

    Create MenuController.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web/Controllers/MenuController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolFoodStamps.Web.Controllers
+{
+    public class MenuController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
