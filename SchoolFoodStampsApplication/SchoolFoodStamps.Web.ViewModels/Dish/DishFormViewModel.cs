9d48d653cc8972406836868ff2aaf19861286433
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-01 22:22:48 +0300

    Create DishFormViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Dish/DishFormViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolFoodStamps.Web.ViewModels.Dish
+{
+    public class DishFormViewModel
+    {
+        public DishFormViewModel()
+        {
+            this.Allergens = new HashSet<string>();
+        }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Weight { get; set; } = string.Empty;
+
+        [Required]
+        public string CateringCompanyId { get; set; } = string.Empty;
+
+        public ICollection<string> Allergens { get; set; }
+    }
+}
