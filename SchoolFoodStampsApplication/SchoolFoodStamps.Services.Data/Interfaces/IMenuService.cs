eb5aa1e7ec7782f2316668a4fdf5063e4a9f7dbf
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-02 23:05:53 +0300

    Create IMenuService.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IMenuService.cs
@@ -0,0 +1,19 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Menu;
+
+namespace SchoolFoodStamps.Services.Data.Interfaces
+{
+    public interface IMenuService
+    {
+        Task<int> CreateAsync(MenuFormViewModel input);
+
+        Task<int> EditAsync(MenuFormViewModel input);
+
+        Task<int> DeleteAsync(int id);
+
+        Task<MenuViewModel> GetByIdAsync(int id);
+
+        Task<IEnumerable<MenuViewModel>> GetAllAsync();
+
+        Task<IEnumerable<MenuViewModel>> GetAllByCateringCompanyAsync(string cateringCompanyId);
+    }
+}
