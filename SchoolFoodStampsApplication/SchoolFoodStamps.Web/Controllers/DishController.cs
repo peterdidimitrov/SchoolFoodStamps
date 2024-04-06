a15e49a7dff8b3fdb2a222a764cba2fc00e0d03d
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-06 11:58:18 +0300

    Implement Index and Add get Methods in DishController.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web/Controllers/DishController.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web/Controllers/DishController.cs
@@ -1,21 +1,51 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SchoolFoodStamps.Services.Data.Interfaces;
+using SchoolFoodStamps.Web.ViewModels.Dish;
+using System.Security.Claims;
 
 namespace SchoolFoodStamps.Web.Controllers
 {
-    [Authorize]
+    [Authorize(Roles = "CateringCompany")]
     public class DishController : BaseController
     {
-        public IActionResult Index()
+        private readonly ILogger<MenuController> logger;
+        private readonly ICateringCompanyService cateringCompanyService;
+        private readonly IDishService dishService;
+        private readonly IAllergenService allergenService;
+
+        public DishController(ILogger<MenuController> logger, ICateringCompanyService cateringCompanyService, IDishService dishService, IAllergenService allergenService)
         {
-            return View();
+            this.logger = logger;
+            this.cateringCompanyService = cateringCompanyService;
+            this.dishService = dishService;
+            this.allergenService = allergenService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string? cateringCompanyId = await cateringCompanyService.GetCateringCompanyIdAsync(User.GetId());
+
+            if (cateringCompanyId == null)
+            {
+                logger.LogError("Catering company not found.");
+                return BadRequest();
+            }
+
+            IEnumerable<DishViewModel> dishes = await this.dishService.GetAllAsync(cateringCompanyId);
+
+            return View(dishes);
         }
 
         [HttpGet]
-        [Authorize(Roles = "CateringCompany")]
         public async Task<IActionResult> Add()
         {
-            return View();
+            DishFormViewModel dishFormViewModel = new DishFormViewModel()
+            {
+                Allergens = await this.allergenService.GetAllAsync()
+            };
+
+            return View(dishFormViewModel);
         }   
     }
 }
