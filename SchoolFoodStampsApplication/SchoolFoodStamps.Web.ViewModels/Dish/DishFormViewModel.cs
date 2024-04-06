596af0220662c3595f85fc242a15c09b7df57cd5
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-06 11:54:02 +0300

    Add validation attributes in DishFormViewModel.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Dish/DishFormViewModel.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Dish/DishFormViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using SchoolFoodStamps.Web.ViewModels.Allergen;
+using System.ComponentModel.DataAnnotations;
+using static SchoolFoodStamps.Common.EntityValidationConstants.ErrorMessages;
+using static SchoolFoodStamps.Common.EntityValidationConstants.Dish;
 
 namespace SchoolFoodStamps.Web.ViewModels.Dish
 {
@@ -6,20 +9,25 @@ namespace SchoolFoodStamps.Web.ViewModels.Dish
     {
         public DishFormViewModel()
         {
-            this.Allergens = new HashSet<string>();
+            this.Allergens = new HashSet<AllergenViewModel>();
         }
-        [Required]
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength, ErrorMessage = ErrorMassageLength)]
         public string Name { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength, ErrorMessage = ErrorMassageLength)]
         public string Description { get; set; } = string.Empty;
 
-        [Required]
+        [Required(ErrorMessage = RequireErrorMessage)]
         public string Weight { get; set; } = string.Empty;
 
-        [Required]
         public string CateringCompanyId { get; set; } = string.Empty;
 
-        public ICollection<string> Allergens { get; set; }
+        [Required(ErrorMessage = RequireErrorMessage)]
+        [Display(Name = "Allergen")]
+        public string AllergenId { get; set; } = string.Empty;
+
+        public IEnumerable<AllergenViewModel> Allergens { get; set; }
     }
 }
