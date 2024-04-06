57509c65fc55d88aa4688da7ab252eb5da50fdf9
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-06 12:02:22 +0300

    Add IAllergenService.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IAllergenService.cs
@@ -0,0 +1,9 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Allergen;
+
+namespace SchoolFoodStamps.Services.Data.Interfaces
+{
+    public interface IAllergenService
+    {
+        Task<IEnumerable<AllergenViewModel>> GetAllAsync();
+    }
+}
