cdce13018e1d88255281687342c126b2e1330275
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-06 11:54:25 +0300

    Create AllergenViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Allergen/AllergenViewMdel.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.Allergen
+{
+    public class AllergenViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+    }
+}
