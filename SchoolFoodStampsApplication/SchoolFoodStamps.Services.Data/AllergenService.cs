b547ce577d0bd1859434deeef1b6242d0aa331e0
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-06 12:01:32 +0300

    Add AllergenService and implement GetAllAsync method.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/AllergenService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolFoodStamps.Data.Common;
+using SchoolFoodStamps.Data.Models;
+using SchoolFoodStamps.Services.Data.Interfaces;
+using SchoolFoodStamps.Web.ViewModels.Allergen;
+
+namespace SchoolFoodStamps.Services.Data
+{
+    public class AllergenService : IAllergenService
+    {
+        private readonly IRepository repository;
+
+        public AllergenService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<IEnumerable<AllergenViewModel>> GetAllAsync()
+        {
+            return await this.repository
+                .AllReadOnly<Allergen>()
+                .Select(a => new AllergenViewModel
+                {
+                    Id = a.Id.ToString(),
+                    Name = a.Name,
+                })
+                .ToListAsync();
+        }
+    }
+}
