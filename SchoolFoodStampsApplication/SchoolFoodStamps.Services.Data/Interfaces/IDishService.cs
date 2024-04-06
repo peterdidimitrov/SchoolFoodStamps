fc8bcec887fb44ab6de81bdfe5b723b8aeff5bb8
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-06 12:02:04 +0300

    Add IDishService.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IDishService.cs
@@ -0,0 +1,19 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Dish;
+
+namespace SchoolFoodStamps.Services.Data.Interfaces
+{
+    public interface IDishService
+    {
+        Task CreateAsync(DishFormViewModel input);
+
+        Task<int> EditAsync(DishFormViewModel input);
+
+        Task<int> DeleteAsync(int id);
+
+        Task<DishViewModel> GetByIdAsync(int id);
+
+        Task<IEnumerable<DishViewModel>> GetAllAsync(string cateringCompanyId);
+
+        Task<IEnumerable<DishViewModel>> GetAllByCateringCompanyAsync(string cateringCompanyId);
+    }
+}
