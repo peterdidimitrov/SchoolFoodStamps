a5e2e8ccb0872abaefdd44f0f39eb610a10f403a
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-07 18:42:39 +0300

    Create AddAllergenToDishFormModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Dish/AddAllergenToDishFormModel.cs
@@ -0,0 +1,16 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Allergen;
+
+namespace SchoolFoodStamps.Web.ViewModels.Dish
+{
+    public class AddAllergenToDishFormModel
+    {
+        public AddAllergenToDishFormModel()
+        {
+            this.Allergens = new List<AllergenViewModel>();
+        }
+
+        public string AllergenId { get; set; } = null!;
+
+        public IEnumerable<AllergenViewModel> Allergens { get; set; }
+    }
+}
