89dfd33d9c5dd8952cc55552bb7681d8c1f46431
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-08 17:00:48 +0300

    Create DishDeleteViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Dish/DishDeleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.Dish
+{
+    public class DishDeleteViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
