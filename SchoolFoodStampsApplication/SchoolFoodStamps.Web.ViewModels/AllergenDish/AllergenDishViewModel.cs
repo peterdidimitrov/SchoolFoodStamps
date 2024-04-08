b8dd6dbda373eb87f18568d6e9d56e1abda9d25a
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-08 14:51:05 +0300

    Create AllergenDishViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/AllergenDish/AllergenDishViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.AllergenDish
+{
+    public class AllergenDishViewModel
+    {
+        public string AllergenId { get; set; } = string.Empty;
+
+        public string DishId { get; set; } = string.Empty;
+
+        public string AllergenName { get; set; } = string.Empty;
+
+        public string DishName { get; set; } = string.Empty;
+    }
+}
