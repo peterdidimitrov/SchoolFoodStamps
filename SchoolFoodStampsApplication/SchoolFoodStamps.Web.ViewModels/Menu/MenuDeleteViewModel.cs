7f89b89325e107a23e2e458a98a0331f22262898
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-09 22:36:05 +0300

    Create MenuDeleteViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Menu/MenuDeleteViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.Menu
+{
+    public class MenuDeleteViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string DayOfWeek { get; set; } = string.Empty;
+    }
+}
