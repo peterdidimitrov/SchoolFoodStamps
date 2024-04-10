7042874abd2ea9e982d61d29b8e63ef0c8e37207
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-10 15:36:12 +0300

    Create DishMenuService and IDishMenuService.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/DishMenuService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolFoodStamps.Data.Common;
+using SchoolFoodStamps.Data.Models;
+using SchoolFoodStamps.Services.Data.Interfaces;
+
+namespace SchoolFoodStamps.Services.Data
+{
+    public class DishMenuService : IDishMenuService
+    {
+        private readonly IRepository repository;
+
+        public DishMenuService(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task AddDishToMenuAsync(Dish dish, Menu menu)
+        {
+            DishMenu dishMenu = new DishMenu
+            {
+                DishId = dish.Id,
+                MenuId = menu.Id
+            };
+
+            dish.DishesMenus.Add(dishMenu);
+            menu.DishesMenus.Add(dishMenu);
+
+            await this.repository.AddAsync(dishMenu);
+            await repository.SaveChangesAsync();
+        }
+
+        public async Task<DishMenu?> GetDishMenuByMenuIdAndDishIdAsync(int dishId, int menuId)
+        {
+            return await this.repository.AllReadOnly<DishMenu>().FirstOrDefaultAsync(x => x.DishId == dishId && x.MenuId == menuId);
+        }
+
+        public async Task RemoveDishFromMenuAsync(Dish dish, Menu menu, DishMenu dishMenu)
+        {
+            dish.DishesMenus.Remove(dishMenu);
+            menu.DishesMenus.Remove(dishMenu);
+
+            this.repository.Delete(dishMenu);
+            await this.repository.SaveChangesAsync();
+        }
+    }
+}
---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Data/Interfaces/IDishMenuService.cs
@@ -0,0 +1,13 @@
+﻿using SchoolFoodStamps.Data.Models;
+
+namespace SchoolFoodStamps.Services.Data.Interfaces
+{
+    public interface IDishMenuService
+    {
+        Task AddDishToMenuAsync(Dish dish, Menu menu);
+
+        Task RemoveDishFromMenuAsync(Dish dish, Menu menu, DishMenu dishMenu);
+
+        Task<DishMenu?> GetDishMenuByMenuIdAndDishIdAsync(int dishId, int menuId);
+    }
+}
