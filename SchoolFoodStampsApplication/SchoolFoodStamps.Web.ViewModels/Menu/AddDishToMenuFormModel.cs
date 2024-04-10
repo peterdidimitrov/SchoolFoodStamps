442be83f15d9a47dac361712df1ab09482d2f3f7
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-10 15:32:26 +0300

    Create AddDishToMenuFormModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Menu/AddDishToMenuFormModel.cs
@@ -0,0 +1,16 @@
+﻿using SchoolFoodStamps.Web.ViewModels.Dish;
+
+namespace SchoolFoodStamps.Web.ViewModels.Menu
+{
+    public class AddDishToMenuFormModel
+    {
+        public AddDishToMenuFormModel()
+        {
+            this.Dishes = new List<DishViewModel>();
+        }
+
+        public string DishId { get; set; } = null!;
+
+        public IEnumerable<DishViewModel> Dishes { get; set; }
+    }
+}
