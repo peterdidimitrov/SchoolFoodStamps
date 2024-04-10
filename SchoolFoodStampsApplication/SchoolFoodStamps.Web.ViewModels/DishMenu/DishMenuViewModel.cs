72befacaaa37040a806692fa3ad2eb5b914d28de
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-10 15:33:18 +0300

    Create DishMenuViewModel.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/DishMenu/DishMenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.DishMenu
+{
+    public class DishMenuViewModel
+    {
+        public string MenuDayOfWeek { get; set; } = string.Empty;
+
+        public string MenuId { get; set; } = string.Empty;
+
+        public string DishId { get; set; } = string.Empty;
+
+        public string DishName { get; set; } = string.Empty;    
+    }
+}
