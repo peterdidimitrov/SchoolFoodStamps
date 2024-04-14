e3bd4167b7a8ceedc15798bbb5d6935d370f2b02
peterdidimitrov <petr_dimitrov@abv.bg>
2024-04-14 23:03:13 +0300

    Seed more data.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Tests/DataBaseSeeder.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Tests/DataBaseSeeder.cs
@@ -8,6 +8,7 @@ namespace SchoolFoodStamps.Services.Tests
     {
         public static List<ApplicationUser> Users = null!;
         public static List<CateringCompany> Companies = null!;
+        public static List<School> Schools = null!;
 
         public static void SeedDataBase(SchoolFoodStampsDbContext dbContext)
         {
@@ -89,19 +90,33 @@ namespace SchoolFoodStamps.Services.Tests
                     Address = null,
                     IdentificationNumber = "121756889",
                     UserId = Guid.Parse("97C32DF3-7A02-49A9-871B-0B27C4C37CB5")
-                },
-                new CateringCompany()
-            {
-                Id = Guid.Parse("EFD31B6C-2A3C-4989-824F-2387C9951235"),
-                Name = "New Company",
-                Address = "New Address",
-                IdentificationNumber = "123456789",
-                UserId = Guid.Parse("AE67ADEF-86A9-4C12-AFFB-457F91A3EE8E"),
-            }
+                }
         };
+            Schools = new List<School>()
+            {
+                new School()
+                {
+                    Id = Guid.Parse("E3AF4B8E-8F07-4962-838E-670BD305758F"),
+                    Name = "41 OU Patriarh Evtimii",
+                    Address = "bul. Hristo Botev 41",
+                    IdentificationNumber = "121756886",
+                    CateringCompanyId = Guid.Parse("EFD31B6C-2A3C-4989-824F-2387C9951234"),
+                    UserId = Guid.Parse("7A40A6C8-B237-4C18-8272-4C8D21C4B5D0")
+                },
+                new School()
+                {
+                    Id = Guid.Parse("6CD00C11-B0CB-428A-9143-DF5743105A92"),
+                    Name = "51 SOU Hristo  Botev",
+                    Address = "bul. Al. Stamboliiski 33",
+                    IdentificationNumber = "121756787",
+                    CateringCompanyId = Guid.Parse("8E91E660-535C-4F3A-B2FB-CC4E28682345"),
+                    UserId = Guid.Parse("D35E9B04-D31B-40F6-8D0D-DA225A969421")
+                },
+            };
 
             dbContext.Users.AddRange(Users);
             dbContext.CateringCompanies.AddRange(Companies);
+            dbContext.Schools.AddRange(Schools);
             dbContext.SaveChanges();
         }
     }
