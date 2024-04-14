adc8f7fe97b2eeb2b1dd3bb406585663852e61f1
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-14 16:34:00 +0300

    Cover more methods in CateringCompanyService with tests.

--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Tests/CateringCompanyServiceTests.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Tests/CateringCompanyServiceTests.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using SchoolFoodStamps.Data;
@@ -7,6 +8,7 @@ using SchoolFoodStamps.Services.Data;
 using SchoolFoodStamps.Services.Data.Interfaces;
 using SchoolFoodStamps.Web.ViewModels.CateringCompany;
 using static SchoolFoodStamps.Services.Tests.DataBaseSeeder;
+using Microsoft.Extensions.Logging;
 
 namespace SchoolFoodStamps.Services.Tests
 {
@@ -15,7 +17,7 @@ namespace SchoolFoodStamps.Services.Tests
         private DbContextOptions<SchoolFoodStampsDbContext> dbOptions;
         private SchoolFoodStampsDbContext dbContext;
         private ICateringCompanyService cateringCompanyService;
-        
+
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -30,10 +32,30 @@ namespace SchoolFoodStamps.Services.Tests
 
             SeedDataBase(this.dbContext);
 
+            var formModel = new CateringCompanyFormViewModel
+            {
+                Id = "97C32DF3-7A02-49A9-871B-0B27C4C37CB5",
+                Name = "New Company Name",
+                Address = "New Company Address",
+                IdentificationNumber = "123456789"
+            };
+
             var repositoryMock = new Mock<IRepository>();
             repositoryMock.Setup(r => r.AllReadOnly<CateringCompany>()).Returns(this.dbContext.CateringCompanies.AsQueryable());
 
-            this.cateringCompanyService = new CateringCompanyService(null, repositoryMock.Object);
+
+            repositoryMock.Setup(r => r.GetByIdAsync<CateringCompany>(formModel.Id))
+    .Returns((object id) => this.dbContext.CateringCompanies.FindAsync(id));
+
+
+            var userManagerMock = new Mock<UserManager<ApplicationUser>>(Mock.Of<IUserStore<ApplicationUser>>(), null!, null!, null!, null!, null!, null!, null!, null!);
+            userManagerMock.Setup(u => u.FindByIdAsync(It.IsAny<string>()))
+                .ReturnsAsync((string userId) =>
+                {
+                    return new ApplicationUser { Id = Guid.Parse(userId), UserName = "testuUser" };
+                });
+
+            this.cateringCompanyService = new CateringCompanyService(userManagerMock.Object, repositoryMock.Object);
         }
 
         [Test]
@@ -46,7 +68,119 @@ namespace SchoolFoodStamps.Services.Tests
             Assert.That(result, Has.Exactly(1).Matches<CateringCompanyViewModel>(
         c => c.Id == "8E91E660-535C-4F3A-B2FB-CC4E28682345".ToLower() && c.Name == "HealtyFoodForChildren"));
 
-            Assert.That(result.Count(), Is.EqualTo(2));
+            Assert.That(result.Count(), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ExistsById_Returns_True_If_Company_Exists()
+        {
+            var result = this.cateringCompanyService.ExistsByIdAsync("EFD31B6C-2A3C-4989-824F-2387C9951234".ToLower()).Result;
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void ExistsById_Returns_False_If_Company_Does_Not_Exist()
+        {
+            var result = this.cateringCompanyService.ExistsByIdAsync("EF431B6C-2A3C-4989-824F-2387C9951235".ToLower()).Result;
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void CreateAsync_Creates_New_Company()
+        {
+            var formModel = new CateringCompanyFormViewModel()
+            {
+                Name = "New Company",
+                Address = "New Address",
+                IdentificationNumber = "123456789",
+                UserId = "AE67ADEF-86A9-4C12-AFFB-457F91A3EE8E",
+                PhoneNumber = "123456789"
+            };
+
+            this.cateringCompanyService.CreateAsync(formModel).Wait();
+
+            var result = this.cateringCompanyService.ExistsByIdentificationNumberAsync("123456789").Result;
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void ExistsByIdentificationNumber_Returns_True_If_Company_Exists()
+        {
+            var result = this.cateringCompanyService.ExistsByIdentificationNumberAsync("121756888").Result;
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void ExistsByIdentificationNumber_Returns_False_If_Company_Does_Not_Exist()
+        {
+            var result = this.cateringCompanyService.ExistsByIdentificationNumberAsync("153456789").Result;
+
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void ExistsByUserId_Returns_True_If_Company_Exists()
+        {
+            var result = this.cateringCompanyService.ExistsByUserIdAsync("FEC4E958-BF56-4247-A6C8-51FAE40D852D").Result;
+
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void ExistsByUserId_Returns_False_If_Company_Does_Not_Exist()
+        {
+            var result = this.cateringCompanyService.ExistsByUserIdAsync("FEC4E958-BF56");
+        }
+
+        [Test]
+        public void GetCateringCompanyByUserIdAsync_Returns_Company_With_Passed_UserId()
+        {
+            var result = this.cateringCompanyService.GetCateringCompanyByUserIdAsync("97C32DF3-7A02-49A9-871B-0B27C4C37CB5").Result;
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result!.Name, Is.EqualTo("HealtyFoodForChildren"));
+            Assert.That(result!.IdentificationNumber, Is.EqualTo("121756889"));
+            Assert.That(result!.Id, Is.EqualTo(Guid.Parse("8E91E660-535C-4F3A-B2FB-CC4E28682345")));
+        }
+
+        [Test]
+        public void GetCateringCompanyByUserIdAsync_Returns_Null_If_Company_Does_Not_Exist()
+        {
+            var result = this.cateringCompanyService.GetCateringCompanyByUserIdAsync("97C32DF3-7A02-49A9-871B-0B27C8C37CB5").Result;
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void UpdateAsync_ShouldUpdateCateringCompany()
+        {
+            var formModel = new CateringCompanyFormViewModel
+            {
+                Id = "97C32DF3-7A02-49A9-871B-0B27C4C37CB5",
+                Name = "New Company Name",
+                Address = "New Company Address",
+                IdentificationNumber = "123456789"
+            };
+
+            this.cateringCompanyService.UpdateAsync(formModel);
+
+            var cateringCompany = this.cateringCompanyService.GetCateringCompanyByUserIdAsync("97C32DF3-7A02-49A9-871B-0B27C4C37CB5").Result;
+
+            Assert.That(formModel.Name, Is.EqualTo(cateringCompany!.Name));
+            Assert.That(cateringCompany.Address, Is.EqualTo(formModel.Address));
+            Assert.That(cateringCompany.IdentificationNumber, Is.EqualTo(formModel.IdentificationNumber));
+            //repositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+        }
+
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            this.dbContext.Database.EnsureDeleted();
         }
     }
 }
--- SchoolFoodStampsApplication/SchoolFoodStamps.Services.Tests/DataBaseSeeder.cs
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Services.Tests/DataBaseSeeder.cs
@@ -1,24 +1,76 @@
 ﻿using SchoolFoodStamps.Data;
 using SchoolFoodStamps.Data.Models;
+using SchoolFoodStamps.Web.ViewModels.CateringCompany;
 
 namespace SchoolFoodStamps.Services.Tests
 {
     public static class DataBaseSeeder
     {
-        public static ApplicationUser User;
-        public static List<CateringCompany> Companies;
+        public static List<ApplicationUser> Users = null!;
+        public static List<CateringCompany> Companies = null!;
 
         public static void SeedDataBase(SchoolFoodStampsDbContext dbContext)
         {
 
-            //User = new ApplicationUser
-            //{
-            //    Id = Guid.Parse("FEC4E958-BF56-4247-A6C8-51FAE40D852D"),
-            //    UserName = "test@test.bg",
-            //    NormalizedUserName = "TEST@TEST.BG",
-            //    Email = "test@test.bg",
-            //    NormalizedEmail = "TEST@TEST.BG"
-            //};
+            Users = new List<ApplicationUser>()
+            {
+                new ApplicationUser()
+            {
+                Id = Guid.Parse("AE67ADEF-86A9-4C12-AFFB-457F91A3EE8E"),
+                UserName = "dimitrichko_admin@org.bg",
+                NormalizedUserName = "DIMITRICHKO_ADMIN@ORG.BG",
+                Email = "dimitrichko_admin@org.bg",
+                NormalizedEmail = "DIMITRICHKO_ADMIN@ORG.BG"
+            },
+                new ApplicationUser()
+            {
+                Id = Guid.Parse("FEC4E958-BF56-4247-A6C8-51FAE40D852D"),
+                UserName = "test@test.bg",
+                NormalizedUserName = "TEST@TEST.BG",
+                Email = "test@test.bg",
+                NormalizedEmail = "TEST@TEST.BG"
+            },
+                new ApplicationUser
+            {
+                Id = Guid.Parse("97C32DF3-7A02-49A9-871B-0B27C4C37CB5"),
+                UserName = "pesho@abv.bg",
+                NormalizedUserName = "PESHO@ABV.BG",
+                Email = "pesho@abv.bg",
+                NormalizedEmail = "PESHO@ABV.BG"
+            },
+                new ApplicationUser
+            {
+                Id = Guid.Parse("7A40A6C8-B237-4C18-8272-4C8D21C4B5D0"),
+                UserName = "patriarh.evtimi@abv.bg",
+                NormalizedUserName = "PATRIARH.EVTIMI@ABV.BG",
+                Email = "patriarh.evtimi@abv.bg",
+                NormalizedEmail = "PATRIARH.EVTIMI@ABV.BG"
+            },
+                new ApplicationUser
+            {
+                Id = Guid.Parse("D35E9B04-D31B-40F6-8D0D-DA225A969421"),
+                UserName = "hristo.botev@abv.bg",
+                NormalizedUserName = "HRISTO.BOTEV@ABV.BG",
+                Email = "hristo.botev@abv.bg",
+                NormalizedEmail = "HRISTO.BOTEV@ABV.BG"
+            },
+                new ApplicationUser
+            {
+                Id = Guid.Parse("F4E56355-18AE-42A7-B082-25A2CF382D3D"),
+                UserName = "pesho@yahoo.com",
+                NormalizedUserName = "PESHO@YAHOO.COM",
+                Email = "pesho@yahoo.com",
+                NormalizedEmail = "PESHO@YAHOO.COM"
+            },
+                new ApplicationUser
+            {
+                Id = Guid.Parse("4AA8654E-1465-4839-814C-A62A69D532E9"),
+                UserName = "gosho@yahoo.com",
+                NormalizedUserName = "GOSHO@YAHOO.COM",
+                Email = "gosho@yahoo.com",
+                NormalizedEmail = "GOSHO@YAHOO.COM"
+            }
+        };
 
             Companies = new List<CateringCompany>()
             {
@@ -37,10 +89,18 @@ namespace SchoolFoodStamps.Services.Tests
                     Address = null,
                     IdentificationNumber = "121756889",
                     UserId = Guid.Parse("97C32DF3-7A02-49A9-871B-0B27C4C37CB5")
-                }
-            };
+                },
+                new CateringCompany()
+            {
+                Id = Guid.Parse("EFD31B6C-2A3C-4989-824F-2387C9951235"),
+                Name = "New Company",
+                Address = "New Address",
+                IdentificationNumber = "123456789",
+                UserId = Guid.Parse("AE67ADEF-86A9-4C12-AFFB-457F91A3EE8E"),
+            }
+        };
 
-            //dbContext.Users.Add(User);
+            dbContext.Users.AddRange(Users);
             dbContext.CateringCompanies.AddRange(Companies);
             dbContext.SaveChanges();
         }
