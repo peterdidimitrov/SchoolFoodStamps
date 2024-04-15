844d98f2f20b183bcfdda5f50c9c413a2c53b873
Peter Dimitrov <113916372+peterdidimitrov@users.noreply.github.com>
2024-04-15 11:00:58 +0300

    StudentDeleteViewModel added.

---
+++ SchoolFoodStampsApplication/SchoolFoodStamps.Web.ViewModels/Student/StudentDeleteViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SchoolFoodStamps.Web.ViewModels.Student
+{
+    public class StudentDeleteViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+    }
+}
